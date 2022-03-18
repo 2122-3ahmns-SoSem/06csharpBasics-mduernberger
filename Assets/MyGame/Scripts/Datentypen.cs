a5f7e6ea658ad4e018f08b83da42d8ef08e2e07c
Melanie <100835056+mduernberger@users.noreply.github.com>
2022-03-18 12:15:05 +0100

    Add files via upload

---
+++ Assets/MyGame/Scripts/Datentypen.cs
@@ -0,0 +1,24 @@
+/*              int
+•	Wertetyp
+•	Speicher: 32 bit
+•	nur ganze Zahlen möglich
+•	Range: -2.147.483.648 bis 2.147.483.647
+•	public int number = 1;
+
+
+                float
+•	Wertetyp
+•	Speicher: 32 bit
+•	dient, um Zahlen zu speichern
+•	Gleitkommazahlen sind auch möglich
+•	bis zu sieben Nachkommazahlen speicherbar
+•	public float number = 1.11f;
+
+
+                bool
+•	Wertetyp
+•	Speicher: 8 bit 
+•	kann nur zwei Werte speichern
+•	wahr und falsch
+•	wird oft mit if-Statements verwendet
+•	public bool question = true; */
